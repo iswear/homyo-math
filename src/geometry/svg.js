47e26241dfb8e54f5b4dd21fa40bd08280c9e899
胡建兵 <471291492@qq.com>
2019-03-07 14:04:04 +0800

    update

--- src/geometry/svg.js
+++ src/geometry/svg.js
@@ -5,49 +5,61 @@ export default (function () {
     this.context = {curPath: null}
   }
 
+  svg.prototype.createSvgTag = function (tagName) {
+    return document.createElementNS('http://www.w3.org/2000/svg', tagName);
+  }
+
   svg.prototype.rect = function (x, y, width, height, style) {
     this.elements.push({
-      el: 'rect',
-      x: x,
-      y: y,
-      width: width,
-      height: height,
-      style: style
+      tag: 'rect',
+      attribute: {
+        x: x,
+        y: y,
+        width: width,
+        height: height,
+        style: style
+      }
     });
     return this;
   }
 
   svg.prototype.circle = function (x, y, radius, style) {
     this.elements.push({
-      el: 'circle',
-      cx: x,
-      cy: y,
-      r: radius,
-      style: style
+      tag: 'circle',
+      attribute: {
+        cx: x,
+        cy: y,
+        r: radius,
+        style: style
+      }
     });
     return this;
   }
 
   svg.prototype.ellipse = function (x, y, radiusX, radiusY, style) {
     this.elements.push({
-      el: 'ellipse',
-      cx: x,
-      cy: y,
-      rx: radiusX,
-      ry: radiusY,
-      style: style
+      tag: 'ellipse',
+      attribute: {
+        cx: x,
+        cy: y,
+        rx: radiusX,
+        ry: radiusY,
+        style: style
+      }
     });
     return this;
   }
 
   svg.prototype.line = function (x1, y1, x2, y2, style) {
     this.elements.push({
-      el: 'line',
-      x1: x1,
-      y1: y1,
-      x2: x2,
-      y2: y2,
-      style: style
+      tag: 'line',
+      attribute: {
+        x1: x1,
+        y1: y1,
+        x2: x2,
+        y2: y2,
+        style: style
+      }
     });
     return this;
   }
@@ -60,9 +72,11 @@ export default (function () {
         pList.push(point[0] + ',' + point[1])
       }
       this.elements.push({
-        el: 'polygon',
-        points: pList.join(' '),
-        style: style
+        tag: 'polygon',
+        attribute: {
+          points: pList.join(' '),
+          style: style
+        }
       })
     }
     return this;
@@ -76,15 +90,25 @@ export default (function () {
         pList.push(point[0] + ',' + point[1])
       }
       this.elements.push({
-        el: 'polyline',
-        points: pList.join(' '),
-        style: style
+        tag: 'polyline',
+        attribute: {
+          points: pList.join(' '),
+          style: style
+        }
       });
     }
   }
 
   svg.prototype.beginPath = function (style) {
-    this.closePath();
+    if (this.context.curPath !== null) {
+      this.elements.push({
+        tag: 'path',
+        attribute: {
+          d: this.context.curPath.commands.join(' '),
+          style: this.context.curPath.style
+        }
+      })
+    }
     this.context.curPath = {
       commands: [],
       style: style
@@ -137,20 +161,59 @@ export default (function () {
 
   svg.prototype.closePath = function () {
     if (this.context.curPath !== null) {
-      this.elements.push({
-        el: 'path',
-        d: this.context.curPath.commands.join(' '),
-        style: this.context.curPath.style
-      });
-      this.context.curPath = null;
+      this.context.curPath.commands.push('Z');
     }
   }
   
-  svg.prototype.toHtmlString = function (x, y, radiusX, radiusY, style) {
-
+  svg.prototype.clear = function () {
+    this.elements = [];
+    this.context = {curPath: null};
+  }
+  
+  svg.prototype.toXmlString = function (withSvgTag) {
+    var nodes = [], elements = this.elements;
+    for (var i = 0, len = elements.length; i < len; ++i) {
+      var element = elements[i];
+      var node = this.createSvgTag(element.tag);
+      var attributes = element.attribute;
+      for (var key in attributes) {
+        var value = attributes[key];
+        if (value !== undefined && value !== null) {
+          node[key] = attributes[key];
+        }
+      }
+      nodes.push(node);
+    }
+    if (this.context.curPath !== null) {
+      var node = this.createSvgTag('path');
+      var attributes = {
+        d: this.context.curPath.commands.join(' '),
+        style: this.context.curPath.style
+      }
+      for (var key in attributes) {
+        var value = attributes[key];
+        if (value !== undefined && value !== null) {
+          node[key] = attributes[key];
+        }
+      }
+      nodes.push(node);
+    }
+    if (withSvgTag) {
+      var root = this.createSvgTag('svg');
+      for (var i = 0, len = nodes.length; i < len; ++i) {
+        root.appendChild(nodes[i]);
+      }
+      return root.outerHTML;
+    } else {
+      var nodeStrs = [];
+      for (var i = 0, len = nodes.length; i < len; ++i) {
+        nodeStrs.push(nodes[i].outerHTML);
+      }
+      return nodeStrs.join('');
+    }
   }
 
   return {
     svg: svg
   }
-})()
+})();
